5f8a2e381965b9e68b5fd29a48fd1637b7ea7977
ehddnr301 <dy950328@gmail.com>
2023-03-29 04:30:26 +0000

    Update start & terminate bash file

--- src/start.sh
+++ src/start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+kubectl create configmap fluentd-config --from-file=fluentd/fluent.conf
+
 kubectl apply -f opensearch/opensearch.yaml
 
 kubectl apply -f ticker/ticker.yaml
 
 kubectl apply -f opensearch_dashboard/open_dashboard.yaml
+
+kubectl apply -f postgresql/postgresql.yaml
--- src/terminate.sh
+++ src/terminate.sh
@@ -5,3 +5,5 @@ kubectl delete -f opensearch/opensearch.yaml
 kubectl delete -f ticker/ticker.yaml
 
 kubectl delete -f opensearch_dashboard/open_dashboard.yaml
+
+kubectl delete -f postgresql/postgresql.yaml
