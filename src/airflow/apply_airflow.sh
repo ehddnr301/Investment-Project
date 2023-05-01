eb3ab1f8ef54a872637024ff37b8ad30c4bb9e87
ehddnr301 <dy950328@gmail.com>
2023-05-01 08:41:50 +0000

    Add airflow data volume yaml

--- src/airflow/apply_airflow.sh
+++ src/airflow/apply_airflow.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sed -i "s/\${SERVER_IP}/$(ifconfig ens4 | grep 'inet ' | awk '{print $2}')/g" airflow.yaml
+sed -i "s/\${SERVER_IP}/$(ifconfig ens4 | grep 'inet ' | awk '{print $2}')/g" airflow_volume.yaml
 
+kubectl apply -f airflow_volume.yaml
 kubectl apply -f airflow.yaml
--- src/airflow/edit_directory_permission.sh
+++ src/airflow/edit_directory_permission.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo chmod -R 777 logs/
+sudo chmod -R 777 data/
