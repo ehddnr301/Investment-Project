cdd9d7c60d4ae32a8c38a22ce32da237b20c37dd
ehddnr301 <dy950328@gmail.com>
2023-05-14 13:42:42 +0000

    Edit Airflow train dag

--- src/airflow/dags/train.py
+++ src/airflow/dags/train.py
@@ -1,6 +1,7 @@
 import os
 
 import time
+import random
 from datetime import datetime, timedelta
 
 import mlflow
@@ -33,14 +34,13 @@ default_args = {
 )
 def train_simple_model(
     code_list: list = ["KRW-BTC", "KRW-ETH"],
-    start_date_time: str = str(datetime.now() - timedelta(hours=48))[:13] + ":00:00",
+    start_date_time: str = str(datetime.now() - timedelta(hours=2))[:13] + ":00:00",
 ):
     @task()
-    def read_train_data_task(code_list: list, start_datetime: str):
+    def read_n_preprocessing_data_task(code_list: list, start_datetime: str):
         postgres_url = (
             "postgresql+psycopg2://postgres:postgres@postgres-service/postgres"
         )
-        start_datetime = str(datetime.now() - timedelta(hours=48))[:13] + ":00:00"
         start_datetime = datetime.strptime(start_datetime, "%Y-%m-%d %H:%M:%S")
         start_unix_datetime = time.mktime(start_datetime.timetuple()) * 1000
 
@@ -56,44 +56,57 @@ def train_simple_model(
         for code in code_list:
             query = sql.format(start_unix_datetime, code)
             df = pd.read_sql(query, con=engine)
-            if not os.path.exists("/opt/airflow/data"):
-                os.makedirs("/opt/airflow/data")
-            df.to_parquet(f"/opt/airflow/data/{code}")
-
-        return "Done"
-
-    @task(trigger_rule=TriggerRule.ALL_SUCCESS)
-    def train_model_task(code_list: list):
-        # Read Data & drop duplicate time Preprocessing
-        for code in code_list:
-            df = pd.read_parquet(f"/opt/airflow/data/{code}")
             df = df.drop_duplicates(subset=["datetime"], keep="last")
             df["datetime2"] = pd.to_datetime(df["datetime"], unit="ms")
-            df["datetime2"] = df["datetime2"].dt.strftime("%Y-%m-%d %H:00:00")
+            df["datetime2"] = df["datetime2"].dt.strftime("%Y-%m-%d %H:%M:00")
             # 시간별로 Aggregation된 Feature 생성
-            df2 = df.groupby("datetime2").agg(
+            agg_df = df.groupby("datetime2").agg(
                 frequent_change=("change", lambda x: x.mode()[0]),
                 average_price=("trade_price", "mean"),
                 total_trade_volume=("trade_volume", "sum"),
             )
-
-            df2["frequent_change"] = df2["frequent_change"].apply(
+            agg_df["frequent_change"] = agg_df["frequent_change"].apply(
                 lambda x: 1 if x == "RISE" else 0
             )
+            agg_df["next_average_price"] = agg_df["average_price"].shift(-1)
+            agg_df.dropna(inplace=True)
+            if not os.path.exists("/opt/airflow/data"):
+                os.makedirs("/opt/airflow/data")
+            agg_df.to_parquet(f"/opt/airflow/data/{code}")
+        return "Done"
+
+    @task(trigger_rule=TriggerRule.ALL_SUCCESS)
+    def train_model_task(code_list: list, start_date_time: str):
+        # Read Data & drop duplicate time Preprocessing
+        for code in code_list:
+            df = pd.read_parquet(f"/opt/airflow/data/{code}")
+
             # train, test set으로 나누기
-            train_data = df2.iloc[:-1]
-            valid_data = df2.iloc[-1:]
+            train_data = df.iloc[:-10]
+            valid_data = df.iloc[-10:]
 
             # Split X, Y
-            X_train = train_data.drop("average_price", axis=1)
-            y_train = train_data["average_price"]
-            X_test = valid_data.drop("average_price", axis=1)
-            y_test = valid_data["average_price"]
+            X_train = train_data.drop("next_average_price", axis=1)
+            y_train = train_data["next_average_price"]
+            X_test = valid_data.drop("next_average_price", axis=1)
+            y_test = valid_data["next_average_price"]
 
-            params = {"n_estimators": 100, "random_state": 42}
+            param_ranges = {
+                "n_estimators": range(50, 101),
+                "max_depth": range(5, 16),
+                "min_samples_split": range(2, 6),
+                "min_samples_leaf": range(1, 6),
+                "max_features": ["auto", "sqrt"],
+            }
+
+            # 랜덤으로 파라미터 선택
+            params = {
+                param: random.choice(param_ranges[param])
+                for param in param_ranges.keys()
+            }
 
             # Model Train
-            model = RandomForestRegressor(**params)
+            model = RandomForestRegressor(random_state=42, **params)
 
             model.fit(X_train, y_train)
 
@@ -112,9 +125,9 @@ def train_simple_model(
                 mlflow.log_metric("MAE", mae)
                 mlflow.sklearn.log_model(model, f"ticker_model_{code}")
 
-    read_data_task = read_train_data_task(code_list, start_date_time)
+    read_data_task = read_n_preprocessing_data_task(code_list, start_date_time)
 
-    train_rf_model_task = train_model_task(code_list)
+    train_rf_model_task = train_model_task(code_list, start_date_time)
 
     read_data_task >> train_rf_model_task
 
