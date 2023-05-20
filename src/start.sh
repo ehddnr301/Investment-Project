3e3a5c361e864ee21bb3c28255cf0efb31a2df56
ehddnr301 <dy950328@gmail.com>
2023-05-20 14:29:51 +0000

    Edit start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
 
+sudo chmod 777 -R minio/
+
 bash start_secret.sh
 
 kubectl create configmap fluentd-config --from-file=fluentd/fluent.conf
-
-kubectl apply -f opensearch/opensearch.yaml
-
-kubectl apply -f ticker/ticker.yaml
-
-kubectl apply -f opensearch_dashboard/open_dashboard.yaml
 
 kubectl apply -f postgresql/postgresql.yaml
 
 sleep 10;
 
+kubectl apply -f opensearch/opensearch.yaml
+
+sleep 10;
+
+kubectl apply -f ticker/ticker.yaml
+
+sleep 10;
+
+kubectl apply -f opensearch_dashboard/open_dashboard.yaml
+
+sleep 180;
+
 cd minio && bash apply_minio.sh && cd ..
+
+sleep 10;
 
 cd mlflow && bash create_database.sh && kubectl apply -f mlflow.yaml && cd ..
 
+sleep 10;
+
 kubectl apply -f fastapi/fastapi.yaml
 
-kubectl apply -f streamlit.yaml
+sleep 10;
+
+kubectl apply -f streamlit/streamlit.yaml
+
+sleep 10;
 
 cd airflow && bash edit_directory_permission.sh && bash apply_airflow.sh
