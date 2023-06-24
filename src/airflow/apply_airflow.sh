9038b15b0a4d59d20aff51445cb3f7f7dddea8c1
ehddnr301 <dy950328@gmail.com>
2023-06-24 17:23:48 +0900

    Edit nfs path
    NFS path 도 변수처리하였습니다.

--- src/airflow/apply_airflow.sh
+++ src/airflow/apply_airflow.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sed -i  "s/\${USER}/$(echo $USER | awk '{print $1}')/g" airflow_volume.yaml
 sed -i "s/\${SERVER_IP}/$(ifconfig ens4 | grep 'inet ' | awk '{print $2}')/g" airflow_volume.yaml
 
 kubectl apply -f airflow_volume.yaml
--- src/minio/apply_minio.sh
+++ src/minio/apply_minio.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sed -i  "s/\${USER}/$(echo $USER | awk '{print $1}')/g" minio.yaml
 sed -i "s/\${SERVER_IP}/$(ifconfig ens4 | grep 'inet ' | awk '{print $2}')/g" minio.yaml
 
 kubectl apply -f minio.yaml
